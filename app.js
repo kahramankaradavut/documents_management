e67ef62ee442c7f4c8b35528e5188493a6236c5f
Kahraman KARADAVUT <117511123+kahramankaradavut@users.noreply.github.com>
2024-09-09 08:03:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const apiRoutes = require('./routes/api');
+const path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use('/api', apiRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
